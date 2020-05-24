a8f8ee479d9bd10c9c6e3384774e38906e69d3ef
Gia Nguyên Ngô <nguyen.ng@teko.vn>
2020-05-24 13:13:31 +0700

    Add film API

--- lib/api/repo/film_repository.dart
+++ lib/api/repo/film_repository.dart
@@ -4,13 +4,13 @@ import 'package:netclick/api/common.dart';
 class FilmRepository {
   static Future<Map<String, dynamic>> getAllFilm({String category}) async {
     Map<String, dynamic> data =
-        await post(url: '/auth/signUp', query: {category: category});
+        await postAuth(url: '/film');
     return data;
   }
 
   static Future<Map<String, dynamic>> getFilmInfo({@required String filmId}) async {
     Map<String, dynamic> data =
-        await post(url: '/auth/signUp/$filmId');
+        await postAuth(url: '/film/$filmId');
     return data;
   }
 }
